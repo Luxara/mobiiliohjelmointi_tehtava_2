ca80d3744a540cd1b8448e007f362e346bcfa358
Lux <luxara-sama@hotmail.fi>
2022-08-27 15:49:18 +0300

    Tehtävä valmis

--- App.js
+++ App.js
@@ -1,10 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput, Alert } from 'react-native';
 
 export default function App() {
+
+  const [number, setNumber]=useState((Math.floor(Math.random() * 100) + 1))
+
+  const[guess, setGuess]=useState('')
+
+  const[result, setResult]=useState('')
+
+  const[tries, setTries]=useState(1)
+
+  const checkGuess = () => {
+    setTries(tries+1) 
+    if(guess==number){
+      Alert.alert('You guessed the number in ' + tries + " guesses!")
+      setNumber((Math.floor(Math.random() * 10) + 1))
+      setTries(1)
+      setResult('')
+    } else if (guess>number) {
+      setResult("Your answer " + guess +" is higher than the number")
+    } else if (guess<number) {
+      setResult("Your answer "+ guess +" is lower than the number")
+    } else{
+      setResult("Please use a number from 1-100")
+    }
+    
+ }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+      <View style={{flex:1, flexDirection:'column', alignItems:'center', justifyContent:'center'}}>
+        <Text>Guess a number between 1-100</Text>
+        <TextInput style={styles.input} keyboardType='number-pad' onChangeText={guess=>setGuess(parseInt(guess))} value={guess}/>
+        <Text>{result}</Text>
+      </View>
+
+      <View style={{flex:1, flexDirection: 'row', alignItems:'center', justifyContent:'space-around', marginTop:'-50%'}}>
+      <Button onPress={checkGuess} title='Make a guess'/>
+      </View>
+      
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +54,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  input:{
+    width:200,
+    borderColor:'gray',
+    borderWidth:1
+  }
 });
